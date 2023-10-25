51402eaf47f55a7716d7e47890dbcd1a01eb06bf
5220aniket <145926907+5220aniket@users.noreply.github.com>
2023-10-25 18:44:20 +0530

    Add files via upload

---
+++ practicecode.py
@@ -0,0 +1,118 @@
+1) # Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+
+2) # Create the variables monthly_savings and num_months
+monthly_savings = 10
+num_months = 4
+
+
+# Multiply monthly_savings and num_months
+new_savings = monthly_savings * num_months
+
+# Add new_savings to your savings
+total_savings =  savings + new_savings
+
+# Print total_savings
+print(total_savings)
+
+
+
+3) # Create a variable half
+half = float(0.5)
+
+# Create a variable intro
+intro = str("Hello! How are you?")
+
+# Create a variable is_good
+is_good = True
+
+
+
+5) monthly_savings = 10
+num_months = 12
+intro = "Hello! How are you?"
+
+# Calculate year_savings using monthly_savings and num_months
+year_savings = monthly_savings * num_months
+
+
+# Print the type of year_savings
+print (type(year_savings))
+
+# Assign sum of intro and intro to doubleintro
+doubleintro = intro + intro
+
+# Print out doubleintro
+print(doubleintro)
+
+
+
+6)# Definition of savings and total_savings
+savings = 100
+total_savings = 150
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(total_savings) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+
+
+
+l1 = ['mathematics','Chemistry',1997,2000]
+l2 = [1,2,3,4,5]
+l3 = l1 + l2
+print(l3)
+
+Subjects = ['Mathematics', 'Chemistry', 'Physics','English','Hindi']
+index = Subjects.index('Chemistry')
+print(index)
+print(Subjects[2])
+
+
+
+Totalmarks = int(input("Enter Marks ="))
+if Totalmarks >= 90:
+    print("Grade A")
+
+elif Totalmarks >= 75:
+
+    print("Grade B")
+
+#elif Totalmarks >= 65:
+    print("Grade C")
+
+else:
+    print("Fail")
+
+print()
+
+
+number = 0
+
+if(number >= 0):
+    if number == 0:
+        print("Number is 0")
+
+    else:
+         print("Number is positive")
+
+else:
+     print("Number is negative")
+
+
+a = "Python"
+for a in "Python":
+    print(a)
+
+
+values = range(11)
+for i in values:
+ print(i)
